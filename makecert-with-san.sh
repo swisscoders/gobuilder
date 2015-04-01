ead0eabba8c3b8493537b9354d75a03085cf8085
Andreas <naepflinandreas@gmail.com>
2015-04-01 21:28:46 +0200

    Repo config: Removed machine specific files
    Removed specific files which are just helpful
    for myself. Such as old web interface, binaries,
    make cert (ssl) helpers and so forth. The repo
    should be clean now.

--- makecert-with-san.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo subjectAltName = IP:127.0.0.1 > cert.cnf
-
-# Private CA
-openssl genrsa -out myCA.key 2048
-openssl req -x509 -new -key myCA.key -out myCA.cer -days 730 -subj /CN="something"
